1fd59fec62e7dbb87cbbc9271bab6222c84a6c96
Vijay Mukhija <mukhijavijay@gmail.com>
2020-06-21 00:17:54 +0530

    Update and delete user API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,15 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword); //will reieve email
 router.patch('/resetPassword/:token', authController.resetPassword); //will recieve token and updated password
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+router.patch(
+  '/updateMyPassword',
+  authController.protect, //this will check the user is logged in or not
+  authController.updatePassword
+);
+
 router
   .route('/')
   .get(userController.getAllUsers)
