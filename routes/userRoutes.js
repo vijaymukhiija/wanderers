3e77e16b631a4662bbab6fdc964e9d75817708e2
Vijay Mukhija <mukhijavijay@gmail.com>
2020-06-27 22:46:37 +0530

    File Upload using multer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,18 +8,23 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
-router.post('/forgotPassword', authController.forgotPassword); //will reieve email
-router.patch('/resetPassword/:token', authController.resetPassword); //will recieve token and updated password
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 // Protect all routes after this middleware
-router.use(authController.protect); //this will check the user is logged in or not & protect all the routes come after this point
+router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
+  userController.updateMe
+);
 router.delete('/deleteMe', userController.deleteMe);
 
-router.use(authController.restrictTo('admin')); //Only admins can create or delete any user
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
