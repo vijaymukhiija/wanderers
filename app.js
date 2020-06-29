f725cb7451cf98ad2c970de69887934f1809c854
Vijay Mukhija <mukhijavijay@gmail.com>
2020-06-29 18:37:42 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1)GLOBAL MIDDLEWARES
+
+//Implement Cors
+app.use(cors());
+app.options('*', cors());
+
+//Serving Static files
 app.use(express.static(`${__dirname}/public`)); //for css img files
 
 //Set Security HTTP Headers
