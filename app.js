1f07920e0c51862a2b77092824a35e78258ca015
Vijay Mukhija <mukhijavijay@gmail.com>
2020-06-29 23:54:42 +0530

    Improved Stripe Implementation

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const bookingController = require('./controllers/bookingController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
@@ -50,6 +51,13 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 //Body Parser,reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
